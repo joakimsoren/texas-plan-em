37fd98fb2ef8b98fc8e311142833d62ef48f3fd7
Joel Ottosson <joel.ottosson@getaccept.com>
2020-10-27 15:43:32 +0100

    fix: median calculation

--- backend/src/stories/stories.helper.ts
+++ backend/src/stories/stories.helper.ts
@@ -2,6 +2,6 @@ export const getMedian = (numbers: number[]): number => {
   const mid: number = Math.floor(numbers.length / 2)
   const sortedNumbers: number[] = [...numbers].sort((a, b) => a - b)
   const isOdd: boolean = numbers.length % 2 !== 0
-  
-  return isOdd ? sortedNumbers[mid] : (sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2
+
+  return isOdd ? sortedNumbers[mid] : Math.floor((sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2)
 }
