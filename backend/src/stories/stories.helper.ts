1ec26c22389ce30f6a7259bd70fc2e2131e0f7c6
Joel Ottosson <joel.ottosson@getaccept.com>
2020-10-27 16:01:23 +0100

    fix: median calculation

--- backend/src/stories/stories.helper.ts
+++ backend/src/stories/stories.helper.ts
@@ -2,6 +2,13 @@ export const getMedian = (numbers: number[]): number => {
   const mid: number = Math.floor(numbers.length / 2)
   const sortedNumbers: number[] = [...numbers].sort((a, b) => a - b)
   const isOdd: boolean = numbers.length % 2 !== 0
+  
+  return isOdd ? sortedNumbers[mid] : roundToNearest((sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2)
+}
 
-  return isOdd ? sortedNumbers[mid] : Math.floor((sortedNumbers[mid - 1] + sortedNumbers[mid]) / 2)
+const roundToNearest = (number: number): number => {
+  const allowedValues: number[] = [1, 2, 4, 8]
+  return allowedValues.reduce(function(prev, curr) {
+    return (Math.abs(curr - number) < Math.abs(prev - number) ? curr : prev);
+  });
 }
